5391c127332469a89d60697edf0e0713248fc030
samuelmwangi <samuelmwangi729@gmail.com>
2019-11-16 16:33:48 +0300

    Final Change

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,7 +16,6 @@
         <link href="{{asset('css/icofont.min.css')}}" rel="stylesheet">
         <link href="{{asset('css/magnific-popup.css')}}" rel="stylesheet">
         <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-        <link href="{{asset('css/app1.css')}}" rel="stylesheet">
         <link href="{{asset('css/appp.css')}}" rel="stylesheet">
         <!-- endinject -->
         <!-- plugin css for this page -->
@@ -30,26 +29,4 @@
         </div>
     </body>
 
-
-
-  <!-- plugins:js -->
-  <script src="vendors/base/vendor.bundle.base.js"></script>
-  <!-- endinject -->
-  <!-- Plugin js for this page-->
-  <script src="{{asset('vendors/chart.js/Chart.min.js')}}"></script>
-  <script src="{{asset('vendors/datatables.net/jquery.dataTables.js')}}"></script>
-  <script src="{{asset('vendors/datatables.net-bs4/dataTables.bootstrap4.js')}}"></script>
-  <!-- End plugin js for this page-->
-  <!-- inject:js -->
-  <script src="{{asset('js/off-canvas.js')}}"></script>
-  <script src="{{asset('js/hoverable-collapse.js')}}"></script>
-  <script src="{{asset('js/template.js')}}"></script>
-
-<script src="{{ asset('js/appp.js') }}" defer></script>
-  <!-- endinject -->
-  <!-- Custom js for this page-->
-  <script src="{{asset('js/dashboard.js')}}"></script>
-  <script src="{{asset('js/data-table.js')}}"></script>
-  <script src="{{asset('js/jquery.dataTables.js')}}"></script>
-  <script src="{{asset('js/dataTables.bootstrap4.js')}}"></script>
 </html>
